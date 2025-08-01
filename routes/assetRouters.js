2b5c26b31f3952857a14b599d6d4a9e3b4a5385e
Ash Nex <ash@nex.com>
2025-08-01 06:29:33 +0000

    Added zod

--- routes/assetRouters.js
+++ routes/assetRouters.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllAssets, getAssetById } from '../controllers/assetController.js';
+import { getAllAssets, getAssetById } from '../controllers/assetControllers.js';
 const router = express.Router();
 
 // router.route('/add')
